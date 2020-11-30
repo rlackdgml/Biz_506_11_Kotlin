a38ddd07f6076d891ea264dcf8c2e38613ef7828
rlackdgml <dhdlqkqh2003@naver.com>
2020-11-30 14:30:53 +0900

    d

---
+++ ImgGallery/app/src/main/java/com/biz/gallery/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.biz.gallery
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import com.biz.gallery.ui.login.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imgView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+
+        val fab : FloatingActionButton=findViewById(R.id.fab)
+        fab.setOnClickListener{ view ->
+
+            // 시스템에 내장된 intent 호출 중에서 가져오기(PICK) 인텐트를 사용하겠다
+            val imgIntent = Intent(Intent.ACTION_PICK)
+
+            // 내장된 컨텐츠를 가져오는 기능
+            imgIntent.type=MediaStore.Images.Media.CONTENT_TYPE
+
+            // 컨텐츠의 저장된 위치 등의 정보를 달라
+            imgIntent.data=MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+            // Activity 를 호출한 후 호출된 activity가 종료할때
+            // 어떤 값을 return 하면 그 값을 수신하겠다  라는 전제하에 사용하는 method
+            // requestCode : 호출하는 Activity에 대한 임의로 설장한 Key값
+            startActivityForResult(imgIntent,1)
+
+        }
+    }
+
+    /**
+     * 호출받은 Activity에서 setResult() method가 실행되고
+     * 결과값을 return 할때 호출되는 event handler
+     * fab의 click evnet에서 사진 갤러리를 open하고 사진을 선택(클릭)했을떄
+     * 그 결과를 수신할 method
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQ_CODE_SELECT_IMAGE){
+            if(resultCode == Activity.RESULT_OK){
+                // 사진 갤러리 activity가 되돌려준 사진정보를 inputstream 으로 변환하여
+                // expecting Property로 변호나시킨다
+                // expecting Property는 변수이름을 백팃으로 감싼다
+                val `in` = contentResolver.openInputStream(data!!.data!!)
+                // BitMap : 픽셀구조로 된 이미지 정보
+                val imgBitMap = BitmapFactory.decodeStream(`in`)
+                imgView.setImageBitmap(imgBitMap)
+                `in`!!.close()
+            }
+        }
+    }
+    // static final 변수를 선언
+    companion object{
+        private const val REQ_CODE_SELECT_IMAGE =1
+    }
+
+    // onCreate()에서 setSupportActionBar 메서드가 실행될때 호출
+    // 햄버거 메뉴를 생성하는 LifeCycle method
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    // 햄버거 메뉴를 확장하고, 항목을 클릭(선택ㄹ)했을떄 발생하는 event
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        /**
+         * app에 여러개의 화면을 구성하여 화면 전환하기 1
+         * 최초로 생성된 프로젝트에는 MainActivity가 있다.
+         * MainActivity는 안드로이드 os 가 호출하여 화면을 구성하는 용도로 사용하는 클래스
+         *
+         * 만약 여러개의 화면(기능)을 가진 app을 만들려면
+         * Activity를 추가한다.
+         * 그러면 AndroidManifest.xml에 Activity가 등록된다.
+         *
+         * main()에서 생성된 Activity를 열기
+         * 1. Intent 클래스를 사용하여  intent객체를 생성
+         *      Intent(누가, 누구를)
+         *      가. java에서 누가 : MainActivity.this, 누구를 LoginActivity.class
+         *      나. Kotlin에서 누가 : this, 누구를 : LoginActivity:: class.java
+         * 2. startActivity(intent)형식으로 open
+         */
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            R.id.action_login -> { // 햄버거 / 로그인메뉴 선택 시
+
+                val loginIntent = Intent(this,LoginActivity::class.java)
+
+                // Intent를 호출할때 어떤 값을 전달하고 싶을떄
+                // putExtra() method를 사용하여 (변수,값) 형식으로 전달한다
+                loginIntent.putExtra("username","dhdlqkqh2003@naver.com")
+                startActivity(loginIntent)
+
+                true
+            }
+            R.id.action_phone ->{
+                val number: String = "tel:010-111-1111"
+
+                /**
+                 * 암시적 intent 호출
+                 * 만약 device에 전화걸기 어플이 다수 설치되어 있을때
+                 * 어플에서 전화걸기 intent를 호출하면 device OS에 설정된 기본 어플을
+                 * 실행하여 전화걸기 화면을 보여주는 것것                 */
+                val phoneCallIntent = Intent(Intent.ACTION_CALL_BUTTON)
+                val phoneIntent = Intent(Intent.ACTION_CALL, Uri.parse(number))
+                startActivity(phoneIntent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
